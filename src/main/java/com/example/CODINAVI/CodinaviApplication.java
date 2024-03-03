0c57a4e150c370b2261a55dab78af85ee4afdaf7
JMJ <ghtntkdnsk@naver.com>
2024-03-04 00:38:41 +0900

    first commit

---
+++ src/main/java/com/example/CODINAVI/CodinaviApplication.java
@@ -0,0 +1,13 @@
+package com.example.CODINAVI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodinaviApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodinaviApplication.class, args);
+	}
+
+}
