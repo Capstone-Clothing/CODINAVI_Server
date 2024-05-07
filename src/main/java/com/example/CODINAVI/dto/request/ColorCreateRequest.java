d29b76bcbd8156f4a803e21464c7c6dc88f51b5f
JMJ <ghtntkdnsk@naver.com>
2024-05-07 11:39:07 +0900

    refactoring

--- src/main/java/com/example/CODINAVI/dto/request/ColorCreateRequest.java
+++ src/main/java/com/example/CODINAVI/dto/request/ColorCreateRequest.java
@@ -5,11 +5,6 @@ public class ColorCreateRequest {
     private String color;
     private String matchColor;
 
-    public ColorCreateRequest(String color, String matchColor) {
-        this.color = color;
-        this.matchColor = matchColor;
-    }
-
     public String getColor() {
         return color;
     }
