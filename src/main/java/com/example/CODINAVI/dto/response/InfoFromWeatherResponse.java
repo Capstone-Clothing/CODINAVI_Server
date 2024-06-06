7c3a965077bb2df5bef8a32d6c3c09a473c4347a
JMJ <ghtntkdnsk@naver.com>
2024-06-07 02:55:30 +0900

    modify: 최저, 최고 기온이 오늘날짜와 같아야 응답할 수 있도록 수정

--- src/main/java/com/example/CODINAVI/dto/response/InfoFromWeatherResponse.java
+++ src/main/java/com/example/CODINAVI/dto/response/InfoFromWeatherResponse.java
@@ -2,8 +2,16 @@ package com.example.CODINAVI.dto.response;
 
 import lombok.Getter;
 
-@Getter
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+
 public class InfoFromWeatherResponse {
+
+    private LocalDateTime nowTime = LocalDateTime.now();
+    private String formatedNow = nowTime.format(DateTimeFormatter.ofPattern("yyyyMMdd HH:mm:ss"));
+    private String subStringNowDay = formatedNow.substring(0, 8);
+    private String subStringNowTime = formatedNow.substring(9, 11);
 
     private String date;
     private String lowTemp;
@@ -12,8 +20,26 @@ public class InfoFromWeatherResponse {
 
     public InfoFromWeatherResponse(String date, String lowTemp, String highTemp, InfoFromTimeResponse info) {
         this.date = date;
-        this.lowTemp = lowTemp;
-        this.highTemp = highTemp;
+        if (date.equals(subStringNowDay)) {
+            this.lowTemp = lowTemp;
+            this.highTemp = highTemp;
+        }
         this.info = info;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getLowTemp() {
+        return lowTemp;
+    }
+
+    public String getHighTemp() {
+        return highTemp;
+    }
+
+    public InfoFromTimeResponse getInfo() {
+        return info;
     }
 }
